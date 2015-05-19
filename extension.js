c85588ef0d90187f883eebe14e396e59420c8246
PremiumPICKLES <canuswim6km@gmail.com>
2015-05-19 15:50:43 -0600

    Update extension.js

--- extension.js
+++ extension.js
@@ -57,13 +57,13 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
         botName: "basicBot",
         language: "english",
-        startupCap: 1, // 1-200
-        startupVolume: 0, // 0-100
-        startupEmoji: false, // true or false
+        startupCap: 25, // 1-200
+        startupVolume: 7, // 0-100
+        startupEmoji: true, // true or false
         cmdDeletion: true,
         chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
         maximumAfk: 120,
-        afkRemoval: true,
+        afkRemoval: false,
         maximumDc: 60,
         bouncerPlus: true,
         blacklistEnabled: true,
